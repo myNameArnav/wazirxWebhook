77cbfc311c3004747afd90848c7600659d033570
Arnav Jain <arnjain3@gmail.com>
2021-09-10 19:47:06 +0530

    tidied up

--- wazirX_API.py
+++ wazirX_API.py
@@ -7,7 +7,7 @@ def getPrice(option):
     
     r = requests.get("https://api.wazirx.com/api/v2/tickers")
     data = json.loads(r.text)
-    # print(r.text)
+    
     if option == "var":
         resp = json.dumps(data, indent=4)
         return resp
@@ -15,5 +15,3 @@ def getPrice(option):
     elif option == "file":
         with open("response.json", "w") as write_file:
             json.dump(data, write_file, indent=4)
-
-getPrice("file")
