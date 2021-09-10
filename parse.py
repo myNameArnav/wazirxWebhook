e3d869c844ec50d553df92768206f0cecb0743ee
Arnav Jain <arnjain3@gmail.com>
2021-09-10 19:47:43 +0530

    made a function and cleaned up

--- parse.py
+++ parse.py
@@ -1,10 +1,11 @@
-from wazirX_API import getPrice
-import json
-import datetime
+def parseJSON(coin):
+    from wazirX_API import getPrice
+    import json
+    import datetime
 
-resp = json.loads(getPrice("var"))
+    resp = json.loads(getPrice("var"))
 
-btcLast = resp["btcusdt"]["last"]
-time = datetime.datetime.fromtimestamp(int(resp["btcusdt"]["at"])).strftime('%Y-%m-%d %H:%M:%S')
-
-print("BTC/USDT at " + time + " was " + btcLast)
+    name = resp[coin]["name"]
+    lastPrice = resp[coin]["last"]
+    time = datetime.datetime.fromtimestamp(int(resp[coin]["at"])).strftime('%Y-%m-%d %H:%M:%S')
+    return name, lastPrice, time
