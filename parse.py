bcf263806ff52866fae2f64d72a283c831f69cec
Arnav Jain <arnjain3@gmail.com>
2021-09-12 16:18:18 +0530

    removed time and unused libraries

--- parse.py
+++ parse.py
@@ -1,11 +1,10 @@
 def parseJSON(coin):
     from wazirX_API import getPrice
     import json
-    import datetime
 
     resp = json.loads(getPrice("var"))
 
     name = resp[coin]["name"]
     lastPrice = resp[coin]["last"]
-    time = datetime.datetime.fromtimestamp(int(resp[coin]["at"])).strftime('%Y-%m-%d %H:%M:%S')
-    return name, lastPrice, time
+    openPrice = resp[coin]["open"]
+    return name, lastPrice, openPrice
