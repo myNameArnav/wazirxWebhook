539b6b984b64d22d9741ae4045f637a7515f1d07
Arnav Jain <arnjain3@gmail.com>
2021-09-12 16:18:53 +0530

    added indicator and color

--- webhook.py
+++ webhook.py
@@ -1,20 +1,34 @@
+from typing import Tuple
 from parse import parseJSON
 from url import *
-from dhooks import Webhook, Embed, embed
+from dhooks import Webhook, Embed
 
 coinList = ["btcusdt", "adainr", "adausdt"]
 
 for i in coinList:
-    namePriceTime = [name, lastPrice, time] = parseJSON(i)
+    allInOne = [name, lastPrice, openPrice] = parseJSON(i)
+    
+    upORdown = str(round(float(lastPrice) - float(openPrice), 3))
+    
+    if float(lastPrice) < float(openPrice):
+        price = " :arrow_down_small:  " + "( " + upORdown + ")"
+        crl = 0xE32636
+    
+    elif float(lastPrice) > float(openPrice):
+        price = " :arrow_up_small: " + "( "+ upORdown + ")"
+        crl = 0x1DB954
+        
+    else:
+        price = lastPrice + " :arrow_forward: "
     
     embeded = Embed(
         description = "Here is the price",
-        color = 0x1DB954
+        color = crl
     )
     
     embeded.set_author(name = "PriceBot")
-    embeded.add_field(name = "Time", value = time)
     embeded.add_field(name = "Name", value = name)
     embeded.add_field(name = "Price", value = lastPrice)
+    embeded.add_field(name = "Change", value = price)
     
     Webhook(bot_url).send(embed=embeded)
