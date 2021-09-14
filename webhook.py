7f39f2404f85f7bc1c11d64c997c627e0314eb9d
Arnav Jain <arnjain3@gmail.com>
2021-09-15 02:23:09 +0530

    added % and fixed width issue

--- webhook.py
+++ webhook.py
@@ -1,3 +1,4 @@
+from os import altsep
 from typing import Tuple
 from parse import parseJSON
 from url import *
@@ -13,28 +14,38 @@ coinDict = {
 for i in coinDict:
     coinDict[i] = [name, lastPrice, openPrice] = (parseJSON(i))
     
-    upORdown = str(round(float(lastPrice) - float(openPrice), 3))
+    lastPrice = float(lastPrice)
+    openPrice = float(openPrice)
+    
+    upORdown = str(round(lastPrice - openPrice, 3))
     
     if float(lastPrice) < float(openPrice):
-        coinDict[i].append(" :arrow_down_small: " + "( " + upORdown + ")")
+        coinDict[i].append(upORdown)
         coinDict[i][0] = "🔴 " + name
         
     elif float(lastPrice) > float(openPrice):
-        coinDict[i].append(" :arrow_up_small: " + "( "+ upORdown + ")")
+        coinDict[i].append("+" + upORdown)
         coinDict[i][0] = "🟢 " + name
         
     else:
         coinDict[i].append (lastPrice + " :arrow_forward: ")
         coinDict[i][0] = "🔵 " + name
-
+    
+    perChange = str(round(((lastPrice - openPrice) / lastPrice)* 100, 2))
+    
+    # if perChange[:-1] not '-':
+    #     perChange = "+" + perChange
+    
+    coinDict[i].append(perChange)
+        
 embedded = Embed(
-    description = "Here is the price",
-)
-embedded.set_author(name = "PriceBot")
+    description = "Here is the prices: ")
 
 for i in coinDict:
-    embedded.add_field(name = "Name", value = coinDict[i][0])
-    embedded.add_field(name = "Price", value = coinDict[i][1])
-    embedded.add_field(name = "Change", value = coinDict[i][3])
+    embedded.add_field(name = "Name", value = coinDict[i][0], inline = False)
+    embedded.add_field(name = "Price", value = coinDict[i][1], inline = True)
+    embedded.add_field(name = "Change", value = coinDict[i][3], inline = True)
+    embedded.add_field(name = "%", value = coinDict[i][4], inline = True)
+    embedded.set_footer(".                                                                                                            .")
     
-Webhook(webhook_url).send(embed=embedded)
+Webhook(bot_url).send(embed=embedded)
