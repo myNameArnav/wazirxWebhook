7e1f639e2c06c19bd414be50c7433432b0484bcc
Steve Sujo <65121688+steveSujo@users.noreply.github.com>
2021-09-18 14:08:47 +0530

    form embedded to Image File : alingment part

--- img.py
+++ img.py
@@ -1,35 +1,78 @@
-from PIL import Image, ImageDraw, ImageFont
-from parse import parseJSON
 
-# * font declaration
-roboto = ImageFont.truetype("fonts\RobotoMono.ttf", 30)
-montserrat = ImageFont.truetype("fonts\Montserrat.ttf", 30)
-bebas = ImageFont.truetype("fonts\BebasNeue.ttf", 45)
-slab = ImageFont.truetype("fonts\RobotoSlab.ttf", 20)
+def responseImg(rates):
+    from PIL import Image, ImageDraw, ImageFont
+    from parse import parseJSON
 
-width = 1280
-height = 720
+    # # * font declaration
+    # roboto = ImageFont.truetype("fonts\RobotoMono.ttf", 30)
+    # montserrat = ImageFont.truetype("fonts\Montserrat.ttf", 30)
+    # bebas = ImageFont.truetype("fonts\BebasNeue.ttf", 45)
+    # slab = ImageFont.truetype("fonts\RobotoSlab.ttf", 20)
 
-# * imported from parse.py
-name, lastPrice, openPrice = parseJSON("btcusdt")
-lastPrice, openPrice = float(lastPrice), float(openPrice)
-change = openPrice - lastPrice
-perChange = str(round(((lastPrice - openPrice) / lastPrice)* 100, 2))
+    # width = 1280
+    # height = 720
 
-img = Image.new(mode = "RGBA", size = (width, height), color = (54, 57, 63, 100))
-draw = ImageDraw.Draw(img)
+    # # * imported from parse.py
+    # for i in rates:
+    #     name, lastPrice, openPrice,uporDown,icon,perChange = rates[i]
+    #     lastPrice, openPrice = float(lastPrice), float(openPrice)
+    #     change = openPrice - lastPrice
+    #     # perChange = str(
+    #     #     round(((lastPrice - openPrice) / lastPrice) * 100, 2))
 
-space = " " * 20
-moreSpace = " " * 25
-spaceFromTop = 60
+    #     img = Image.new(mode="RGBA", size=(
+    #         width, height), color=(54, 57, 63, 100))
+    #     draw = ImageDraw.Draw(img)
 
-hdr = "Name" + space + "Price" + space + "Change" + space + "%"
-draw.text((10,5), hdr, font = bebas, fill = (225,225,225), align= 'right')
+    # space = " " * 20
+    #     moreSpace = " " * 25
+    #     spaceFromTop = 60
 
-txt = name + moreSpace + str(lastPrice) + moreSpace + str(change) + moreSpace + str(perChange)
+    #     hdr = "Name" + space + "Price" + space + "Change" + space + "%"
+    #     draw.text((10, 5), hdr, font=bebas, fill=(
+    #         225, 225, 225), align='right',embedded_color=True)
 
-draw.text((10,spaceFromTop), txt, font = slab, fill=(255,255,255), align = 'right')
+    #     txt = icon +moreSpace+ name + moreSpace + \
+    #         str(lastPrice) + moreSpace + \
+    #         str(change) + moreSpace + str(perChange)
 
-img.save("picture.png")
+    #     draw.text((10, spaceFromTop), txt, font=slab,
+    #               fill=(255, 255, 255), align='right')
+    # img.save("picture.png")
 
-img.show()
+    # img.show()
+
+    fnt = ImageFont.truetype("fonts\HackNF_Regular.ttf", 45, layout_engine=ImageFont.LAYOUT_RAQM)
+
+    img = Image.new(mode="RGBA", size=(
+        1280, 720), color=(54, 57, 63, 100))
+    draw = ImageDraw.Draw(img)
+    nameList="Name\n" 
+    lastPriceList="lastPrice\n"
+    up_DownList="uporDown\n"
+    perChangeList="perChange\n"
+    iconsList="\n"
+    colors=[(255,255,255)]
+    for i in rates:
+        name, lastPrice, openPrice,uporDown,arrow,colorFill,perChange = rates[i]
+        colors.append(colorFill) 
+
+        iconsList=iconsList+arrow+"\n"
+        nameList= nameList + name +"\n"
+        lastPriceList=lastPriceList +lastPrice +"\n"
+        up_DownList= up_DownList + uporDown + "\n"
+        perChangeList= perChangeList + perChange + "\n"
+
+        
+
+
+
+    draw.multiline_text((50,100),iconsList,font=fnt,fill=colors[1],align='left',spacing=25)
+    draw.multiline_text((100,100),nameList,font=fnt,fill=colors[0],align='left',spacing=25)
+    draw.multiline_text((350,100),lastPriceList,font=fnt,fill=colors[0],align='left',spacing=25)
+    draw.multiline_text((620,100),up_DownList,font=fnt,fill=colors[0],align='left',spacing=25)
+    draw.multiline_text((870,100),perChangeList,font=fnt,fill=colors[0],align='left',spacing=25)
+
+    img.save("picture.png")
+    img.show()
+    # return img
--- webhook.py
+++ webhook.py
@@ -2,50 +2,60 @@ from os import altsep
 from typing import Tuple
 from parse import parseJSON
 from url import *
-from dhooks import Webhook, Embed
+from dhooks import Webhook, File
+from img import responseImg
 
 # coinList = ["btcusdt", "adainr", "adausdt"]
 
 coinDict = {
-    "btcusdt":[],
-    "adausdt":[]
+    "btcusdt": [],
+    "adausdt": []
 }
 
 for i in coinDict:
-    coinDict[i] = [name, lastPrice, openPrice] = (parseJSON(i))
-    
+    coinDict[i] = [name, lastPrice, openPrice] = parseJSON(i)
+
     lastPrice = float(lastPrice)
     openPrice = float(openPrice)
-    
+
     upORdown = str(round(lastPrice - openPrice, 3))
-    
+
     if float(lastPrice) < float(openPrice):
         coinDict[i].append(upORdown)
-        coinDict[i][0] = "🔴 " + name
-        
+        coinDict[i].append("")
+        coinDict[i].append((255,0,0))
+
     elif float(lastPrice) > float(openPrice):
         coinDict[i].append("+" + upORdown)
-        coinDict[i][0] = "🟢 " + name
-        
+        coinDict[i].append("")
+        coinDict[i].append((0,255,0))
+
     else:
-        coinDict[i].append (lastPrice + " :arrow_forward: ")
-        coinDict[i][0] = "🔵 " + name
-    
-    perChange = str(round(((lastPrice - openPrice) / lastPrice)* 100, 2))
-    
+        coinDict[i].append(lastPrice + " :arrow_forward: ")
+        coinDict[i].append("")
+        coinDict[i].append((0,0,255))
+
+    perChange = str(round(((lastPrice - openPrice) / lastPrice) * 100, 2))
+
     # if perChange[:-1] not '-':
     #     perChange = "+" + perChange
-    
-    coinDict[i].append(perChange)
-        
-embedded = Embed(
-    description = "Here is the prices: ")
 
-for i in coinDict:
-    embedded.add_field(name = "Name", value = coinDict[i][0], inline = False)
-    embedded.add_field(name = "Price", value = coinDict[i][1], inline = True)
-    embedded.add_field(name = "Change", value = coinDict[i][3], inline = True)
-    embedded.add_field(name = "%", value = coinDict[i][4], inline = True)
-    embedded.set_footer(".                                                                                                            .")
-    
-Webhook(webhook_url).send(embed=embedded)
+    coinDict[i].append(perChange)
+
+responseImg(coinDict)
+
+hook = Webhook(webhook_url)
+imgFile = File("picture.png", name="output.png")
+# # embedded = Embed(
+# #     description="Here is the prices: ")
+
+# # for i in coinDict:
+# #     embedded.add_field(name="Name", value=coinDict[i][0], inline=False)
+# #     embedded.add_field(name="Price", value=coinDict[i][1], inline=True)
+# #     embedded.add_field(name="Change", value=coinDict[i][3], inline=True)
+# #     embedded.add_field(name="%", value=coinDict[i][4], inline=True)
+# #     embedded.set_footer(
+# #         ".                                                                                                            .")
+
+# # embedded.send(imgFile)
+hook.send("Yo sup", file=imgFile)
